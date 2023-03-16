78b0e304626512550d30a235d35642df2c2e1f99
“codewitherrors” <codewitherrors1@gmail.com>
2023-03-16 13:08:22 +0530

    Add : Edit Contact

--- AddressBookDB.sql
+++ AddressBookDB.sql
@@ -19,3 +19,9 @@ insert into Address_Book values
 
 --View contacts in address book
 select * from Address_Book;
+
+
+--UC4 Ability to edit existing contact person using their name
+update Address_Book set City='Ghaziabad' where First_Name='Anmol';
+update Address_Book set City='Ghaziabad' where First_Name='Manisha';
+update Address_Book set City='Ghaziabad' where First_Name='Piyush';
